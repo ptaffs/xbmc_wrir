edcb75a8e19294f265a6aedb44ba0a0b2bd0781b
peter taffs <peter@flatmac.home>
2014-07-06 22:00:26 -0400

    json call to get current show
    gets current show

--- wrir.py
+++ wrir.py
@@ -1,13 +1,20 @@
 import sys
 import xbmcgui
 import xbmcplugin
+import urllib2, json
 
 addon_handle = int(sys.argv[1])
-
 xbmcplugin.setContent(addon_handle, 'audio')
 
-url = 'http://wrir.serverroom.us:7722/wrir'
-li = xbmcgui.ListItem('Listen Live', iconImage='http://wrir.org/assets/img/logo.gif')
-xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+url="http://files.wrir.org/cgi-bin/rightnow"
+result=json.load(urllib2.urlopen(url))
+
+print result['title'];
+print result['poster'];
+
+livestream = 'http://wrir.serverroom.us:7722/wrir'
+li = xbmcgui.ListItem('Stream: ' + result['title'],
+ iconImage='http://wrir.org/' + result['poster'])
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=livestream, listitem=li)
 
 xbmcplugin.endOfDirectory(addon_handle)
