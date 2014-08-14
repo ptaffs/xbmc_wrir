a3320a609af6eb27539426bc2c74e164b05cd038
peter taffs <peter@flatmac.home>
2014-08-14 19:02:45 -0400

    with archived shows

--- wrir.py
+++ wrir.py
@@ -6,8 +6,8 @@ import urllib2, json
 addon_handle = int(sys.argv[1])
 xbmcplugin.setContent(addon_handle, 'audio')
 
-url="http://files.wrir.org/cgi-bin/rightnow"
-result=json.load(urllib2.urlopen(url))
+rightnowurl="http://files.wrir.org/cgi-bin/rightnow"
+result=json.load(urllib2.urlopen(rightnowurl))
 
 print result['title'];
 print result['poster'];
@@ -17,4 +17,10 @@ li = xbmcgui.ListItem('Stream: ' + result['title'],
  iconImage='http://wrir.org/' + result['poster'])
 xbmcplugin.addDirectoryItem(handle=addon_handle, url=livestream, listitem=li)
 
+
+showsurl="http://files.wrir.org/cgi-bin/sl2"
+result=json.load(urllib2.urlopen(showsurl))
+for i in result['shows']:
+          li=xbmcgui.ListItem(i['title'],iconImage='http://wrir.org/'+i['poster'])
+          xbmcplugin.addDirectoryItem(handle=addon_handle,url=i['mp3'],listitem=li)
 xbmcplugin.endOfDirectory(addon_handle)
